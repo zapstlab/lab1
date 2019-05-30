8caf844b3248e72d091730a7b6d525fafb6a1613
Dariusz Bursztynowski <dariusz.bursztynowski@orange.com>
2019-05-30 13:02:36 +0200

    Update openrc-example.sh

--- openrc-example.sh
+++ openrc-example.sh
@@ -1,5 +1,5 @@
 export OS_PROJECT_NAME=cloudify-test
-export OS_USERNAME=username
-export OS_PASSWORD=password
+export OS_USERNAME=os-username
+export OS_PASSWORD=os-password
 export OS_AUTH_URL=http://192.168.186.11:5000/v3
 export OS_IDENTITY_API_VERSION=3
